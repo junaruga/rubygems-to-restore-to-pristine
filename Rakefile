07737ada4ec093f79760fdea5469b597b59d3787
Jun Aruga <jaruga@redhat.com>
2019-09-04 20:01:47 +0200

    Add run method with the test.

--- Rakefile
+++ Rakefile
@@ -1,6 +1,8 @@
 require 'rake/testtask'
 Rake::TestTask.new do |t|
   t.test_files = FileList['test/**/*_test.rb']
+  t.warning = true
+  t.verbose = true
 end
 
 require 'rubocop/rake_task'
--- test/gem_restorer_test.rb
+++ test/gem_restorer_test.rb
@@ -10,11 +10,7 @@ class GemRestorerTest < Minitest::Test
 
   def test_each
     restorer = GemRestorer.new
-    stub_string = <<~MESSAGE
-      Ignoring byebug-11.0.1 because its extensions are not built. Try: gem pristine byebug --version 11.0.1
-      Ignoring nio4r-2.5.1 because its extensions are not built. Try: gem pristine nio4r --version 2.5.1
-    MESSAGE
-    restorer.stub :gem_pristine_warnings, stub_string do
+    restorer.stub :gem_pristine_warnings, stub_gem_warnings do
       h = {}
       restorer.each do |name, version|
         h[name] = version
@@ -23,5 +19,31 @@ class GemRestorerTest < Minitest::Test
       assert_equal h['byebug'], '11.0.1'
       assert_equal h['nio4r'], '2.5.1'
     end
+  end
+
+  def test_run
+    restorer = GemRestorer.new
+    restorer.stub :gem_pristine_warnings, stub_gem_warnings do
+      out_cmds = []
+      stub_sh = proc do |cmd|
+        out_cmds << cmd
+      end
+      restorer.stub :sh, stub_sh do
+        restorer.run
+        assert_equal out_cmds.size, 4
+        assert_equal out_cmds[0], 'gem uninstall -x byebug'
+        assert_equal out_cmds[1], 'gem install byebug -v 11.0.1 --user-install'
+        assert_equal out_cmds[2], 'gem uninstall -x nio4r'
+        assert_equal out_cmds[3], 'gem install nio4r -v 2.5.1 --user-install'
+      end
+    end
+  end
+
+  def stub_gem_warnings
+    stub_string = <<~MESSAGE
+      Ignoring byebug-11.0.1 because its extensions are not built. Try: gem pristine byebug --version 11.0.1
+      Ignoring nio4r-2.5.1 because its extensions are not built. Try: gem pristine nio4r --version 2.5.1
+    MESSAGE
+    stub_string
   end
 end
